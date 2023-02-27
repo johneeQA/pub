c2666b8ed89919495bdc47684ade5724211ae141
Денис <120407020+johneeQA@users.noreply.github.com>
2023-02-27 23:51:12 +0300

    Add files via upload

---
+++ fp 28/settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    chrome_driver = 'C:\chromedriver/chromedriver.exe'
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'engine87@mail.ru'
+    valid_password = '121212Zz'
+    valid_email_for_reg = 'engine87@mail.ru'
+    invalid_email = '87engine@mail.ru'
+    invalid_password = 'Zz121212'
+    empty_email = ''
+    empty_password = ''
+    menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    f_name = 'Кк'
+    l_name = 'Дд'
+    f_name_ = '-'
+    l_name_ = '-'
+    russian_generate_string = 'и'
+    latin_generate_string = 'ssttww'
+    chinese_chars = '我不会说汉语请做不辣的怎么样你会说英语'
+    special_chars = '!@#$$%^&*()_+_+=//{}'
+    numbers = 987
+    empty = ''
+    passw1 = 'qQ654321'
+    passw2 = 'Qw12345678Wq543'
+    passw3 = 'Qa12345678Er12345978'
